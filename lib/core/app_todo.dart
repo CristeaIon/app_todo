64d3c6f232bb85d4b79093f0c9e9a579e938632b
Ion <cristeaion36@gmail.com>
2024-03-21 20:40:50 +0200

    setting up app localization

--- lib/core/app_todo.dart
+++ lib/core/app_todo.dart
@@ -1,5 +1,8 @@
 import 'package:app_todo/core/app_todo_theme.dart';
+import 'package:app_todo/core/l10n/generated/l10n.dart';
+import 'package:app_todo/core/splash/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppTodo extends StatelessWidget {
   const AppTodo({super.key});
@@ -8,7 +11,16 @@ class AppTodo extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AppTodo',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        L.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: L.delegate.supportedLocales,
       theme: AppTodoTheme.black(),
+      home: const UpTodoSplash(),
     );
   }
 }
