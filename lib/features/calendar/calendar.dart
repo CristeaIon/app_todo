b63f04e529c84a44ca2e8bace0eecb074177d058
Ion <cristeaion36@gmail.com>
2024-04-05 22:01:21 +0300

    added titles to calendar, focus and profile screens

--- lib/features/calendar/calendar.dart
+++ lib/features/calendar/calendar.dart
@@ -1,12 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../core/l10n/generated/l10n.dart';
+
 @RoutePage()
 class CalendarScreen extends StatelessWidget {
   const CalendarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Text(
+          L.of(context).calendar,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
   }
 }
--- lib/features/profile/profile.dart
+++ lib/features/profile/profile.dart
@@ -1,12 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../core/l10n/generated/l10n.dart';
+
 @RoutePage()
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Text(
+          L.of(context).profile,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
   }
 }
